2b40437e1ef54c49ba360e4b8a294009467f0430
tlautensc <34093354+tlautensc@users.noreply.github.com>
2022-09-17 12:46:07 +0200

    Added Smoothnes

--- Backend/Main.py
+++ Backend/Main.py
@@ -2,9 +2,10 @@ from Position import Position
 import copy
 
 activePos = Position(0,0,0,0,0,0,0)
+targetPos = Position(10,0,0,0,0,0,0)
 presetPos = []
 time = 0
-smoothnes = 0
+smoothnes = 1
 
 for i in range(16):
     presetPos.append(Position(0,0,0,0,0,0,0))
@@ -14,9 +15,50 @@ def presetSave(i):
     presetPos[i] = copy.deepcopy(activePos)
 
 def presetLoad(i):
-    global activePos
-    activePos = copy.deepcopy(presetPos[i])
+    global targetPos
+    targetPos = copy.deepcopy(presetPos[i])
     execute()
 
 def execute():
-    pass
+    stepcount = 20
+    steps = []
+
+    for i in range(stepcount):
+        steps.append(smooth((1/stepcount)*i,smoothnes))
+
+    dCranePan = targetPos.cranePan - activePos.cranePan
+    dCraneTilt = targetPos.craneTilt - activePos.craneTilt
+    dCraneTele = targetPos.craneTele - activePos.craneTele
+    dCamPan = targetPos.camPan - activePos.camPan
+    dCamTilt = targetPos.camTilt - activePos.camTilt
+    dCamZoom = targetPos.camZoom - activePos.camZoom
+    dCamFocus = targetPos.camFocus - activePos.camFocus
+    stepsCranePan = []
+    stepsCraneTilt = []
+    stepsCraneTele = []
+    stepsCamPan = []
+    stepsCamTilt = []
+    stepsCamZoom = []
+    stepsCamFocus = []
+
+    for i in range(stepcount):
+        stepsCranePan.append(dCranePan*steps[i])
+        stepsCraneTilt.append(dCraneTilt*steps[i])
+        stepsCraneTele.append(dCraneTele*steps[i])
+        stepsCamPan.append(dCamPan*steps[i])
+        stepsCamTilt.append(dCamTilt*steps[i])
+        stepsCamZoom.append(dCamZoom*steps[i])
+        stepsCamFocus.append(dCamFocus*steps[i])
+        
+
+
+def smooth(x,m):
+    n = 2*m+1
+    if x<0.5:
+        y=(pow(2,n-1))*pow(x,n)
+    else:
+        y=1-(pow((-2*x+2),n))/2
+    return y
+
+
+execute()
